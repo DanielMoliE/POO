2b6f802043da6b14563a5a470535269442f1da86
Daniel Molina <dmolinae1900@alumno.ipn.mx>
2020-10-18 00:46:46 -0500

    descubri como invocar scanner

--- Menu_operaciones.java
+++ Menu_operaciones.java
@@ -1,7 +1,16 @@
-
+import java.util.Scanner;
 
 public class Menu_operaciones{
     public static void main(String[] args) {
         System.out.println("Programa de menu de operaciones");
+        
+        Scanner casio = new Scanner(System.in);
+
+        int valor1, valor2, resultado;
+
+        
+        System.out.println("Introduce los numeros a sumar");
+        
+        
     }
 }
