fe36bfc0e0bb48b0c76d1dee44710c2e7d1f6910
Daniel Molina <dmolinae1900@alumno.ipn.mx>
2020-10-18 01:33:36 -0500

    Se agrego un menu con el switch y ademas de agregar otra operacion resta

--- Menu_operaciones.java
+++ Menu_operaciones.java
@@ -7,13 +7,32 @@ public class Menu_operaciones{
         Scanner casio = new Scanner(System.in);
 
         float valor1, valor2, resultado;
+        int opcion;
 
+        System.out.println("Elegir una de las opciones");
+        System.out.println("1.-Sumar numeros");
+        System.out.println("2.-Restar numeros");
+        opcion = casio.nextInt();
         
-        System.out.println("Introduce los numeros a sumar");
-        valor1 = casio.nextFloat(); 
-        valor2 = casio.nextFloat();
-        resultado = valor1 + valor2;
-        System.out.println("La suma de los dos numeros es de: "+resultado);  
+        switch(opcion){
+            case 1:
+                System.out.println("Introduce los numeros a sumar");
+                valor1 = casio.nextFloat(); 
+                valor2 = casio.nextFloat();
+                resultado = valor1 + valor2;
+                System.out.println("La suma de los dos numeros es de: "+resultado);
+                break;
+            case 2:
+                System.out.println("Introduce los numeros a restar");
+                valor1 = casio.nextFloat(); 
+                valor2 = casio.nextFloat();
+                resultado = valor1 - valor2;
+                System.out.println("La resta de los 2 numeros es de: "+resultado);
+                break;
+
+        default: 
+            System.out.println("Se acabo tu mugre programa");
+        }  
     }
      
 }
