f87564dda45c9b06edfd30469751168cef0d2832
Daniel Molina <dmolinae1900@alumno.ipn.mx>
2020-10-20 17:26:23 -0500

    Pruebas de nombre

--- Menu_operaciones.java
+++ Menu_operaciones.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Menu_operaciones{
-    
+
     public static void main(String[] args) {
-        System.out.println("Programa de menu de operaciones");
+        System.out.println("Programa de menu de operaciones haciendo un cambio");
         
         Scanner casio = new Scanner(System.in);
 
