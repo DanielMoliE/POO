1891acd40ca699d24d531b254957e10c546d6645
Daniel Molina <dmolinae1900@alumno.ipn.mx>
2020-10-26 02:04:47 -0600

    Expansion del ejemplo Hola Mundo

--- Holamundo.java
+++ Holamundo.java
@@ -1,7 +1,48 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Holamundo {
  
      public static void main(String[] args) {
         System.out.println("Hola mundo");
+        long caca = 10;
+        caca = 15;
+        System.out.println("numero "+caca);
+        final float caca2 = 1.25f;
+        System.out.println("numero "+caca2);
+        char cacaracter = 'a';
+        System.out.println("caracter "+cacaracter);
+        Integer caca3 = null;
+        System.out.println("valor "+caca3);
+        String cacalenguaje = "Milanesa";
+        System.out.println("palabra "+cacalenguaje);
+        Scanner Balu = new Scanner(System.in);
         
+        
+        String cadena;
+        int entero;
+        Double decimalp;
+        float decimal;
+
+        cadena = JOptionPane.showInputDialog("Introduzca una cadena ");
+        entero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un entero"));
+        decimalp = Double.parseDouble(JOptionPane.showInputDialog("Intudoce un numero decimal"));
+        decimal = Float.parseFloat(JOptionPane.showInputDialog("Introduce un decimal "));
+
+        JOptionPane.showMessageDialog(null, "Tu cadena es "+cadena);
+        JOptionPane.showMessageDialog(null, "Tu entero es "+entero);
+        JOptionPane.showMessageDialog(null, "Tu decimal es "+decimalp);
+        JOptionPane.showMessageDialog(null, "Tu decimal 2 es "+decimal);
+
+        Float caca4;
+        System.out.println("Introduce un valor");
+            caca4 = Balu.nextFloat();
+        System.out.println("Tu valor "+caca4);
+        char caca5;
+        System.out.println("Introduce una letra");
+            caca5 = Balu.next().charAt(0);
+            System.out.println("Tu letra es "+caca5);
+
     }
 }
---
+++ Menu_ejercicios_3IV9_Molina_Escobar_Daniel.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Menu_ejercicios_3IV9_Molina_Escobar_Daniel{
+    public static void main(String[] args) {
+        System.out.println("Programa de menu de operaciones");
+        
+        Scanner casio = new Scanner(System.in);
+
+        float valor1, valor2, valor3, valor4;
+        double resultado, resultado2;
+        int opcion;
+        do{
+        System.out.println("Elegir una de las opciones");
+        System.out.println("1.-Ingresar el precio del abono y la edad");
+        System.out.println("2.-Calcular el perimetro y el área de un triangulo");
+        System.out.println("3.-Calcular el perimetro y el área de un circulo");
+        System.out.println("4.-Calcular el perimetro y el área de un rectangulo");
+        System.out.println("5.-Calcular el perimetro y el área de un rombo");
+        System.out.println("6.-Calcular el perimetro y el área de un hexagono");
+        System.out.println("7.-Salir");
+        opcion = casio.nextInt();
+        
+        switch(opcion){
+            case 1:
+                System.out.println("Introduce el precio del abono");
+                valor1 = casio.nextFloat(); 
+                System.out.printl("Introduce tu edad");
+                valor2 = casio.nextfloat();
+                
+                System.out.println("El perimetro del cuadrado es: "+resultado2 +" Unidades");
+                System.out.println("El área del cuadrado es de: "+resultado +" Unidades cuadradas");
+                break;
+            case 2:
+                System.out.println("Introduce la base");
+                valor1 = casio.nextFloat();
+                System.out.println("Introduce los otros 2 lados"); 
+                valor2 = casio.nextFloat();
+                valor3 = casio.nextFloat();
+                System.out.println("Introduce la altura");
+                valor4 = casio.nextFloat();
+                resultado = (valor1 * valor4)/2;
+                resultado2= valor1 + valor2 + valor3;
+                System.out.println("El perimetro del triangulo es: "+resultado2 +" Unidades");
+                System.out.println("El área del triangulo es: "+resultado +" Unidades cuadradas");
+                break;
+
+            case 3:
+                System.out.println("Introduce el radio del circulo");
+                valor1 = casio.nextFloat();
+                resultado = 3.1416*(valor1*valor1);
+                resultado2= 3.1416*(valor1+valor1);
+                System.out.println("El perimetro del circulo: "+resultado2 + " Unidades");
+                System.out.println("El área del circulo es: "+resultado + " Unidades cuadradas");
+                break;
+            case 4:
+                System.out.println("Introduce la base y la altura del rectangulo");
+                valor1 = casio.nextFloat();
+                valor2 = casio.nextFloat();
+                resultado= valor1 * valor2;
+                System.out.println("El área del rectangulo es: "+resultado + " Unidades cuadradas");
+                break;
+            case 5:
+                System.out.println("Introduce la distancia mayor y la distancia menor del rombo");
+                valor1 = casio.nextFloat();
+                valor2 = casio.nextFloat();
+                resultado= (valor1 * valor2)/2;
+                System.out.println("El área del rectangulo es: "+resultado + " Unidades cuadradas");
+                break;
+            case 6:
+                System.out.println("Introduce el perimetro y el apotema del hexagono");
+                valor1 = casio.nextFloat();
+                valor2 = casio.nextFloat();
+                resultado= (valor1*valor2)/2;
+                System.out.println("El área del hexagono es: "+resultado + " Unidades cuadradas");
+            case 7:
+                System.out.println("Ojalá te mueras");
+                break;
+            default:
+            System.out.println("Se acabo tu mugre programa");
+            }
+        }while(opcion !=7);
+            
+        }  
+    }
+     
